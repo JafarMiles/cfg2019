d6d7c7b250f826346ec3b8e8aefafd0fccd13eb5
Jafar Miles <jafar@accurx.com>
2019-11-18 23:38:31 +0000

    init

---
+++ Lesson5/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, request
+from datetime import datetime
+
+app = Flask("ToyApp")
+
+statusList = [{'status':'hello world', 'username': 'Jaf', 'time': '18:40'}, {'status': 'yes please', 'username': 'Jaf', 'time': '18:42'}]
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html', feed=statusList)
+
+
+@app.route("/addnote", methods=['POST'])
+def addnote():
+    form_data = request.form
+    status = form_data['status']
+    username = form_data['username']
+    time = datetime.now().strftime('%H:%M');
+    statusList.append({'status': status, 'username': username, 'time':time})
+    return redirect('/')
+
+
+app.run(debug=True)
+
+
+# use ngrok!
